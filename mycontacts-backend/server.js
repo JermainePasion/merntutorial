613141fc42d348cb06cfbeaabbd1c4fae2e86aa0
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2024-12-22 23:38:15 +0800

    mongodb integration (mongoose)

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorhandler');
+const connectDb = require('./config/dbConnection');
 const dotevnv = require('dotenv').config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
